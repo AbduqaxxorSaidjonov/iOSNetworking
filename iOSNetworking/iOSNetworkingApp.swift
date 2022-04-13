b9cd0b7ef6c077516ff438a25d009554b2663901
GitHub <abduqahhor2349@gmail.com>
2022-04-14 03:39:57 +0500

    Employee

--- iOSNetworking/iOSNetworkingApp.swift
+++ iOSNetworking/iOSNetworkingApp.swift
@@ -13,7 +13,7 @@ struct iOSNetworkingApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainScreen()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
